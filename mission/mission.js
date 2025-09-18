8a84020642bcc5c896ffeb2fd235fea8cf7a48e5
byronmckitrick16 <mck24036@byui.edu>
2025-09-18 12:07:02 -0600

    making it look nicer

--- mission/mission.js
+++ mission/mission.js
@@ -1,7 +1,6 @@
 const themeSelector = document.querySelector("#theme");
 function changeTheme() {
-// check to see what the current value of our select is.
-// The current value is conveniently found in themeSelector.value!
+
     if(themeSelector.value === "dark"){
         document.body.setAttribute("class", "dark");
 
@@ -16,14 +15,6 @@ function changeTheme() {
         byuiLogo.removeAttribute("src");
         byuiLogo.setAttribute("src", "byui-logo_blue.webp");
     }
-// if the value is dark then:
-// add the dark class to the body
-// change the source of the logo img to point to the white logo.
-// otherwise
-// remove the dark class
-// make sure the logo src is the blue logo.
 }
 
-// add an event listener to the themeSelector element here.
-// Use the changeTheme function as the event handler function.
 themeSelector.addEventListener('change', changeTheme);
